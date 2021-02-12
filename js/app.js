3fc188d275b70900297671173d7487f96b61f995
elisa33 <pipi.tdfuego@gmail.com>
2021-02-12 18:56:30 -0400

    show alerts

--- js/app.js
+++ js/app.js
@@ -23,3 +23,36 @@ function buscarClima(e) {
 	//consultar la api
 	consultarAPI(ciudad, pais);
 }
+
+function mostrarError(mensaje) {
+	const alerta = document.querySelector('.bg-red-100');
+	if (!alerta) {
+		//crear alerta
+		const alerta = document.createElement('div');
+
+		alerta.classList.add(
+			'bg-red-100',
+			'border-red-400',
+			'text-red-700',
+			'px-4',
+			'py-3',
+			'rounded',
+			'max-w-md',
+			'mx-auto',
+			'mt-6',
+			'text-center'
+		);
+
+		alerta.innerHTML = `
+    <strong class="font-bold">Error!</strong>
+    <span class='block'>${mensaje}</span>
+    `;
+
+		container.appendChild(alerta);
+
+		//se elimina la alerta despues de 5 segundo
+		setTimeout(() => {
+			alerta.remove();
+		}, 5000);
+	}
+}
